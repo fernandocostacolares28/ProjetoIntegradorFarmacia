1fc7d419dddb418701a6c8bc0e6c3c04d8e27d41
Fernando Costa Colares <fcc281002@gmail.com>
2023-11-07 15:42:36 -0300

    commit12

---
+++ Entidade/EntidadeCliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoIntegradorFarmacia.Entidade
+{
+    internal class EntidadeCliente
+    {
+        public Int32 id_cliente;
+        public string name_cliente;
+        public string cpf_cliente;
+        public string telefone_cliente;
+        public string endereco_cliente;
+    }
+}
